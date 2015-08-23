722a6bd487b1c03deb829a131969c5586ad0877f
ffreylaw <ffreylaw@geoffreys-mbp.iinet>
2015-08-23 12:54:03 +1000

    fibonacci
    fibonacci

--- fib-1.c
+++ fib-1.c
@@ -3,27 +3,27 @@
 #include <ctype.h>
 #include <string.h>
 
+void convert_str2num(char *str, double *num);
 double fib(double n);
 
 int main(int argc, char *argv[]) {
-    int i;
-    double n, result;
+    double n=0;
     if (argc!=2) {
         printf("Invalid input\n");
         exit(EXIT_FAILURE);
     }
-    for(i=0; i<((int)strlen(argv[1])); i++) {
-        if (isdigit(argv[1][i])==1) {
-            n = n*10+(argv[1][i]-'0');
+    convert_str2num(argv[1], &n);
+    printf("Fibonacci: %.f\n", fib(n));
+    return 0;
+}
+
+void convert_str2num(char *str, double *num) {
+    int i;
+    for(i=0; i<((int)strlen(str)); i++) {
+        if (isdigit(str[i])==1) {
+            *num = *num*10+(str[i]-'0');
         }
     }
-    if (n==0) {
-        result = 0;
-    } else {
-        result = fib(n);
-    }
-    printf("Fibonacci: %.f\n", result);
-    return 0;
 }
 
 double fib(double n) {
--- fib-2.c
+++ fib-2.c
@@ -3,33 +3,35 @@
 #include <ctype.h>
 #include <string.h>
 
+void convert_str2num(char *str, double *num);
 double (*fib(double (*M)[2], double n))[2];
 double (*sqm(double (*A)[2], double (*B)[2]))[2];
-double M[2][2]={{1,1},{1,0}};
+double M[2][2]={{1,1},{1,0}}, Z[2][2]={{0,0},{0,0}};
 
 int main(int argc, char *argv[]) {
-    int i;
-    double n, result;
+    double n=0;
     if (argc!=2) {
         printf("Invalid input\n");
         exit(EXIT_FAILURE);
     }
-    for(i=0; i<((int)strlen(argv[1])); i++) {
-        if (isdigit(argv[1][i])==1) {
-            n = n*10+(argv[1][i]-'0');
-        }
-    }
-    if (n==0) {
-        result = 0;
-    } else {
-        result = (fib(M, n))[1][0];
-    }
-    printf("Fibonacci: %.f\n", result);
+    convert_str2num(argv[1], &n);
+    printf("Fibonacci: %.f\n", (fib(M, n))[1][0]);
     return 0;
 }
 
+void convert_str2num(char *str, double *num) {
+    int i;
+    for(i=0; i<((int)strlen(str)); i++) {
+        if (isdigit(str[i])==1) {
+            *num = *num*10+(str[i]-'0');
+        }
+    }
+}
+
 double (*fib(double (*M)[2], double n))[2] {
-    if (n==1) {
+    if (n==0) {
+        return Z;
+    } else if (n==1) {
         return M;
     } else {
         return sqm(fib(M, n-1), M);
