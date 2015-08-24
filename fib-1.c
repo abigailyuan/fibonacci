b432ac378769961e92226b5a6a4450e3cd9b9171
ffreylaw <ffreylaw@Geoffreys-MacBook-Pro.local>
2015-08-24 14:30:55 +1000

    fibonacci
    fibonacci

--- fib-1.c
+++ fib-1.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-void convert_str2num(char *str, double *num);
 double fib(double n);
 
 int main(int argc, char *argv[]) {
-    double n=0;
     if (argc!=2) {
         printf("Invalid input\n");
         exit(EXIT_FAILURE);
     }
-    convert_str2num(argv[1], &n);
-    printf("Fibonacci: %.f\n", fib(n));
+    printf("Fibonacci: %.f\n", fib(atoi(argv[1])));
     return 0;
-}
-
-void convert_str2num(char *str, double *num) {
-    int i;
-    for(i=0; i<((int)strlen(str)); i++) {
-        if (isdigit(str[i])==1) {
-            *num = *num*10+(str[i]-'0');
-        }
-    }
 }
 
 double fib(double n) {
--- fib-2.c
+++ fib-2.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-void convert_str2num(char *str, double *num);
-double (*fib(double (*M)[2], double n))[2];
-double (*sqm(double (*A)[2], double (*B)[2]))[2];
-double M[2][2]={{1,1},{1,0}}, Z[2][2]={{0,0},{0,0}};
+#define N 2
+
+double (*fib(double (*M)[N], double n))[N];
+double (*sqm(double (*A)[N], double (*B)[N]))[N];
+double M[N][N]={{1,1},{1,0}}, Z[N][N]={{0,0},{0,0}};
 
 int main(int argc, char *argv[]) {
-    double n=0;
     if (argc!=2) {
         printf("Invalid input\n");
         exit(EXIT_FAILURE);
     }
-    convert_str2num(argv[1], &n);
-    printf("Fibonacci: %.f\n", (fib(M, n))[1][0]);
+    printf("Fibonacci: %.f\n", (fib(M, atoi(argv[1])))[1][0]);
     return 0;
 }
 
-void convert_str2num(char *str, double *num) {
-    int i;
-    for(i=0; i<((int)strlen(str)); i++) {
-        if (isdigit(str[i])==1) {
-            *num = *num*10+(str[i]-'0');
-        }
-    }
-}
-
-double (*fib(double (*M)[2], double n))[2] {
+double (*fib(double (M[])[N], double n))[N] {
     if (n==0) {
         return Z;
     } else if (n==1) {
@@ -38,8 +26,8 @@ double (*fib(double (*M)[2], double n))[2] {
     }
 }
 
-double (*sqm(double (*A)[2], double (*B)[2]))[2] {
-    double C[2][2];
+double (*sqm(double (A[])[N], double (B[])[N]))[N] {
+    double C[N][N];
     C[0][0] = (A[0][0]*B[0][0])+(A[0][1]*B[1][0]);
     C[0][1] = (A[0][0]*B[0][1])+(A[0][1]*B[1][1]);
     C[1][0] = (A[1][0]*B[0][0])+(A[1][1]*B[1][0]);
