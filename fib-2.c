69ddf4194dc62636878a0aa31221d3c00508a55d
ffreylaw <ffreylaw@Geoffreys-MacBook-Pro.local>
2015-08-26 12:56:10 +1000

    fibonacci
    fibonacci

--- fib-2.c
+++ fib-2.c
@@ -3,34 +3,34 @@
 
 #define N 2
 
-double (*fib(double (M[])[N], double n))[N];
-double (*sqm(double (A[])[N], double (B[])[N]))[N];
-double M[N][N]={{1,1},{1,0}}, Z[N][N]={{0,0},{0,0}};
+double (*FIBO(double (M[])[N], double n))[N];
+double (*SQMM(double (A[])[N], double (B[])[N]))[N];
+double M[N][N]={{1,1},{1,0}}, ZERO[N][N]={{0,0},{0,0}};
 
-int main(int argc, char *argv[]) {
+int main(int argc, const char * argv[]) {
     if (argc!=2) {
         printf("Invalid input\n");
         exit(EXIT_FAILURE);
     }
-    printf("Fibonacci: %.f\n", (fib(M, atoi(argv[1])))[1][0]);
+    printf("Fibonacci: %.f\n", (FIBO(M, atoi(argv[1])))[1][0]);
     return 0;
 }
 
-double (*fib(double (M[])[N], double n))[N] {
+double (*FIBO(double (M[])[N], double n))[N] {
     if (n==0) {
-        return Z;
+        return ZERO;
     } else if (n==1) {
         return M;
     } else {
-        return sqm(fib(M, n-1), M);
+        return SQMM(FIBO(M, n-1), M);
     }
 }
 
-double (*sqm(double (A[])[N], double (B[])[N]))[N] {
-    double C[N][N];
-    C[0][0] = (A[0][0]*B[0][0])+(A[0][1]*B[1][0]);
-    C[0][1] = (A[0][0]*B[0][1])+(A[0][1]*B[1][1]);
-    C[1][0] = (A[1][0]*B[0][0])+(A[1][1]*B[1][0]);
-    C[1][1] = (A[1][0]*B[0][1])+(A[1][1]*B[1][1]);
-    return C;
+double (*SQMM(double (A[])[N], double (B[])[N]))[N] {
+    double F[N][N];
+    F[0][0] = (A[0][0]*B[0][0])+(A[0][1]*B[1][0]);
+    F[0][1] = (A[0][0]*B[0][1])+(A[0][1]*B[1][1]);
+    F[1][0] = (A[1][0]*B[0][0])+(A[1][1]*B[1][0]);
+    F[1][1] = (A[1][0]*B[0][1])+(A[1][1]*B[1][1]);
+    return F;
 }
