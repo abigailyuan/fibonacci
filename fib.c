a00ad3415afdbaa52bf8b42cfd9b307a2d6730b8
ffreylaw <ffreylaw@Geoffreys-MacBook-Pro.local>
2015-09-23 18:47:59 +1000

    fibonacci
    fibonacci

--- fib.c
+++ fib.c
@@ -2,62 +2,48 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <assert.h>
 
 typedef struct {
     double r1c1;
     double r1c2;
     double r2c1;
     double r2c2;
-} Qm_t;
+} qm_t;
 
-int str_isdigit(char *str);
-Qm_t fib_qm(int n);
-Qm_t sqmm(Qm_t M1, Qm_t M2); // square matrix multiplication
+qm_t fib(int n);
+qm_t sqmm(qm_t M1, qm_t M2);
 
 int main(int argc, char * argv[]) {
     int n;
     if (!argv[1]) {
         printf("Usage: %s integer\n", argv[0]);
         exit(EXIT_FAILURE);
-    } else if (!str_isdigit(argv[1])) {
-        printf("Usage: %s integer\n", argv[0]);
-        exit(EXIT_FAILURE);
     } else {
         n = atoi(argv[1]);
+        assert(n);
     }
-    printf("%.0f\n", fib_qm(n).r1c2);
+    printf("%.0f\n", fib(n-1).r1c1);
 }
 
-int str_isdigit(char *str) {
-    int i, flag=1;
-    for (i=0; i<strlen(str); i++) {
-        if (!isdigit(str[i])) {
-            flag = 0;
-        } else {
-            continue;
-        }
-    }
-    return flag;
-}
-
-Qm_t fib_qm(int n) {
-    Qm_t Q={1, 1, 1, 0}, Z={0, 0, 0, 0};
+qm_t fib(int n) {
+    qm_t Q={1,1,1,0}, Z={0,0,0,0};
     if (n==0) {
         return Z;
     } else if (n==1) {
         return Q;
-    } else if ((n>1)&&(n%2==0)) {  // even n
-        return sqmm(fib_qm(n/2), fib_qm(n/2));
-    } else if ((n>1)&&(n%2!=0)) {  // odd n
-        return sqmm(sqmm(fib_qm((n-1)/2), fib_qm((n-1)/2)), Q);
+    } else if ((n>1)&&(n%2==0)) {
+        return sqmm(fib(n/2), fib(n/2));
+    } else if ((n>1)&&(n%2!=0)) {
+        return sqmm(sqmm(fib((n-1)/2), fib((n-1)/2)), Q);
     } else {
-        printf("Error\n");
+        fprintf(stderr, "Error\n");
         exit(EXIT_FAILURE);
     }
 }
 
-Qm_t sqmm(Qm_t M1, Qm_t M2) {
-    Qm_t M;
+qm_t sqmm(qm_t M1, qm_t M2) {
+    qm_t M;
     M.r1c1 = M1.r1c1*M2.r1c1+M1.r1c2*M2.r2c1;
     M.r1c2 = M1.r1c1*M2.r1c2+M1.r1c2*M2.r2c2;
     M.r2c1 = M1.r2c1*M2.r1c1+M1.r2c2*M2.r2c1;
